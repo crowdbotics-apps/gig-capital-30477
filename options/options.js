07dff32d3d3fb16edb901a99b9808eb1a09f1314
Crowdbotics <team@crowdbotics.com>
2021-09-12 08:44:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "gig_capital_30477Identifier",
+  url: "https://gig_capital_30477Identifier.botics.co",
+  api: "https://gig_capital_30477Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
